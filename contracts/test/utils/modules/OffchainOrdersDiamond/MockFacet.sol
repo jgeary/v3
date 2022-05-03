73a928a9ee8cc2c73ecff8aaddd5946a4f95b4a1
James Geary <36774175+jgeary@users.noreply.github.com>
2022-05-03 06:05:26 -0400

    initial diamond implementation with permanent selectors

---
+++ contracts/test/utils/modules/OffchainOrdersDiamond/MockFacet.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.10;
+
+import {LibDiamond} from "../../../common/DiamondPermanentSelectors/libraries/LibDiamond.sol";
+import {MockStorage} from "./MockStorage.sol";
+
+contract MockFacet {
+    MockStorage internal s;
+
+    event DataSet(bool foo, uint8 bar);
+
+    function setData(bool _foo, uint8 _bar) external {
+        LibDiamond.enforceIsContractOwner();
+        s.foo = _foo;
+        s.bar = _bar;
+        emit DataSet(_foo, _bar);
+    }
+
+    function getData() external view returns (bool, uint8) {
+        return (s.foo, s.bar);
+    }
+}
